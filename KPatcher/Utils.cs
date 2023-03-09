f47e3e9bfb6d598da2c6d417957a8b8e37dba097
SilveIT <silverice.yuriy@gmail.com>
2023-03-09 21:33:29 +0300

    Got tired of having fun with generating UserProfileInfo in a weird way, switched to JSON

--- KPatcher/Utils.cs
+++ KPatcher/Utils.cs
@@ -4,8 +4,11 @@ using System.Globalization;
 using System.Linq;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 using System.Windows;
 using HarmonyLib;
+using KPatcher.Properties;
 using Microsoft.Win32;
 using Application = System.Windows.Forms.Application;
 
@@ -83,7 +86,7 @@ namespace KPatcher
         {
             try
             {
-                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
                 if (registryKey == null)
                     throw new NullReferenceException("Registry Run key is null");
                 var isInStartup = registryKey.GetValue(Program.TargetName) != null;
@@ -107,11 +110,10 @@ namespace KPatcher
         /// Emulates successful KrispSDKResponse&lt;T&gt; instance, sets response data
         /// </summary>
         /// <param name="dataInstance"></param>
-        /// <param name="krispDataType"></param>
         /// <returns></returns>
-        public static (object, Type) MakeKrispResponse(object dataInstance, Type krispDataType)
+        public static (object, Type) MakeKrispResponse(object dataInstance)
         {
-            var krispRespType = R.T[0].MakeGenericType(krispDataType);
+            var krispRespType = R.T[0].MakeGenericType(dataInstance.GetType());
             var krispResp = Activator.CreateInstance(krispRespType);
             var krispRespDataProp = AccessTools.Property(krispRespType, "data");
             krispRespDataProp.SetValue(krispResp, dataInstance);
@@ -171,16 +173,16 @@ namespace KPatcher
         {
             var result = new List<CultureInfo>();
 
-            ResourceManager rm = new ResourceManager("Krisp.Properties.Resources", Program.AssemblyLoader.RequestedAssembly);
+            var rm = new ResourceManager("Krisp.Properties.Resources", Program.AssemblyLoader.RequestedAssembly);
 
             var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-            foreach (CultureInfo culture in cultures)
+            foreach (var culture in cultures)
             {
                 try
                 {
                     if (culture.Equals(CultureInfo.InvariantCulture)) continue; //do not use "==", won't work
 
-                    ResourceSet rs = rm.GetResourceSet(culture, true, false);
+                    var rs = rm.GetResourceSet(culture, true, false);
                     if (rs != null)
                         result.Add(culture);
                 }
@@ -227,7 +229,7 @@ namespace KPatcher
 
                 instInfSettingsProp.SetValue(installationInfo, listOfSettings);
 
-                return MakeKrispResponse(installationInfo, R.T[2]);
+                return MakeKrispResponse(installationInfo);
             }
 
             public static (object, Type) GenerateInstallationResponseREST(object installationRequestInfo)
@@ -237,29 +239,7 @@ namespace KPatcher
             }
         }
 
-        [PropertyRequired("Krisp.BackEnd.Mode", "props", "UserProfileInfo Generator")]                              //0
-        [PropertyRequired("Krisp.BackEnd.Mode", "name", "UserProfileInfo Generator")]                               //1
-        [PropertyRequired("Krisp.BackEnd.Team", "id", "UserProfileInfo Generator")]                                 //2
-        [PropertyRequired("Krisp.BackEnd.Team", "name", "UserProfileInfo Generator")]                               //3
-        [PropertyRequired("Krisp.BackEnd.UpdateSetting", "prevent_update", "UserProfileInfo Generator")]            //4
-        [PropertyRequired("Krisp.BackEnd.NCBalance", "balance", "UserProfileInfo Generator")]                       //5
-        [PropertyRequired("Krisp.BackEnd.NCOutSetting", "krisp_mic_as_default", "UserProfileInfo Generator")]       //6
-        [PropertyRequired("Krisp.BackEnd.NCOutSetting", "minutes_settings", "UserProfileInfo Generator")]           //7
-        [PropertyRequired("Krisp.BackEnd.NCOutSetting", "headset_vc", "UserProfileInfo Generator")]                 //8
-        [PropertyRequired("Krisp.BackEnd.NCOutSetting", "ultrasonic", "UserProfileInfo Generator")]                 //9
-        [PropertyRequired("Krisp.BackEnd.BaseProfileSetting", "available", "UserProfileInfo Generator")]            //10
-        [PropertyRequired("Krisp.BackEnd.ProfileSettings", "contact_support", "UserProfileInfo Generator")]         //11
-        [PropertyRequired("Krisp.BackEnd.ProfileSettings", "report_problem", "UserProfileInfo Generator")]          //12
-        [PropertyRequired("Krisp.BackEnd.ProfileSettings", "update", "UserProfileInfo Generator")]                  //13
-        [PropertyRequired("Krisp.BackEnd.ProfileSettings", "nc_in", "UserProfileInfo Generator")]                   //14
-        [PropertyRequired("Krisp.BackEnd.ProfileSettings", "nc_out", "UserProfileInfo Generator")]                  //15
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "email", "UserProfileInfo Generator")]                   //16
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "id", "UserProfileInfo Generator")]                      //17
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "mode", "UserProfileInfo Generator")]                    //18
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "team", "UserProfileInfo Generator")]                    //19
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "settings", "UserProfileInfo Generator")]                //20
-        [PropertyRequired("Krisp.BackEnd.UserProfileInfo", "ref_string", "UserProfileInfo Generator")]              //21
-        [PropertyRequired("Krisp.BackEnd.HeadsetVcSetting", "available", "UserProfileInfo Generator")]              //22
+        [TypeRequired("Krisp.BackEnd.UserProfileInfo", "UserProfileInfo Generator")]
         public static class UserProfileInfoGenerator
         {
             // ReSharper disable InconsistentNaming
@@ -270,87 +250,8 @@ namespace KPatcher
             /// <returns></returns>
             public static (object, Type) GenerateUserProfileInfoKrisp(bool updateEnabled)
             {
-                var modePropsProp = R.P[0]; //"Krisp.BackEnd.Mode", "props"
-                var modeNameProp = R.P[1]; //"Krisp.BackEnd.Mode", "name"
-                var teamIDProp = R.P[2]; //"Krisp.BackEnd.Team", "id"
-                var teamNameProp = R.P[3]; //"Krisp.BackEnd.Team", "name"
-                var updateSettingPreventUpdateProp = R.P[4]; //"Krisp.BackEnd.UpdateSetting", "prevent_update"
-
-                var NCBalanceBalanceProp = R.P[5];
-                var HeadsetVCAvailableProp = R.P[22];
-                //var NCOutSettingMicDefaultProp = R.P[6]; //"Krisp.BackEnd.NCOutSetting", "krisp_mic_as_default"
-                var NCOutSettingMinutesProp = R.P[7]; //"Krisp.BackEnd.NCOutSetting", "minutes_settings"
-                var NCOutSettingHeadsetVCProp = R.P[8]; //"Krisp.BackEnd.NCOutSetting", "headset_vc"
-                //var NCOutSettingUltrasonicProp = R.P[9]; //"Krisp.BackEnd.NCOutSetting", "ultrasonic"
-
-                var baseSettingAvailableProp = R.P[10]; //"Krisp.BackEnd.BaseProfileSetting", "available"
-                var profileSettingsContactSupportProp = R.P[11]; //"Krisp.BackEnd.ProfileSettings", "contact_support"
-                var profileSettingsReportProblemProp = R.P[12]; //"Krisp.BackEnd.ProfileSettings", "report_problem"
-                var profileSettingsUpdateProp = R.P[13]; //"Krisp.BackEnd.ProfileSettings", "update"
-                var profileSettingsNCInProp = R.P[14]; //"Krisp.BackEnd.ProfileSettings", "nc_in"
-                var profileSettingsNCOutProp = R.P[15]; //"Krisp.BackEnd.ProfileSettings", "nc_out"
-                var NCInStateProp = AccessTools.Property(profileSettingsNCInProp.PropertyType, "state");
-                var NCOutStateProp = AccessTools.Property(profileSettingsNCOutProp.PropertyType, "state");
-                var infoEmailProp = R.P[16]; //"Krisp.BackEnd.UserProfileInfo", "email"
-                var infoIdProp = R.P[17]; //"Krisp.BackEnd.UserProfileInfo", "id"
-                var infoModeProp = R.P[18]; //"Krisp.BackEnd.UserProfileInfo", "mode"
-                var infoTeamProp = R.P[19]; //"Krisp.BackEnd.UserProfileInfo", "team"
-                var infoSettingsProp = R.P[20]; //"Krisp.BackEnd.UserProfileInfo", "settings"
-                var infoRefStringProp = R.P[21]; //"Krisp.BackEnd.UserProfileInfo", "ref_string"
-
-                var userProfileInfo = Activator.CreateInstance(R.T[16]); //"Krisp.BackEnd.UserProfileInfo", "email"
-                var mode = Activator.CreateInstance(R.T[0]); //"Krisp.BackEnd.Mode", "props"
-                var team = Activator.CreateInstance(R.T[2]); //"Krisp.BackEnd.Team", "id"
-                var settings = Activator.CreateInstance(R.T[11]); //"Krisp.BackEnd.ProfileSettings", "contact_support"
-                var contact_support = Activator.CreateInstance(R.T[10]); //"Krisp.BackEnd.BaseProfileSetting", "available"
-                var report_problem = Activator.CreateInstance(R.T[10]); //"Krisp.BackEnd.BaseProfileSetting", "available"
-                var update = Activator.CreateInstance(R.T[4]); //"Krisp.BackEnd.UpdateSetting", "prevent_update"
-                var nc_in = Activator.CreateInstance(profileSettingsNCInProp.PropertyType); //Was different type in the old version of the target app
-                var nc_out = Activator.CreateInstance(profileSettingsNCOutProp.PropertyType);
-
-                //var krisp_mic_as_default = Activator.CreateInstance();
-                var minutes_settings = Activator.CreateInstance(R.T[5]); //"Krisp.BackEnd.NCBalance", "balance"
-                var headset_vc = Activator.CreateInstance(R.T[22]);
-                //var ultrasonic = Activator.CreateInstance();
-
-                NCBalanceBalanceProp.SetValue(minutes_settings, 1337U);
-                HeadsetVCAvailableProp.SetValue(headset_vc, true);
-
-                //NCOutSettingMicDefaultProp.SetValue(nc_out, krisp_mic_as_default);
-                NCOutSettingMinutesProp.SetValue(nc_out, minutes_settings);
-                NCOutSettingHeadsetVCProp.SetValue(nc_out, headset_vc);
-                //NCOutSettingUltrasonicProp.SetValue(nc_out, ultrasonic);
-
-                baseSettingAvailableProp.SetValue(nc_out, true);
-                baseSettingAvailableProp.SetValue(update, updateEnabled);
-                baseSettingAvailableProp.SetValue(report_problem, true);
-                baseSettingAvailableProp.SetValue(contact_support, true);
-
-                NCOutStateProp?.SetValue(nc_out, "user_choice");
-                NCInStateProp?.SetValue(nc_in, "user_choice");
-
-                updateSettingPreventUpdateProp.SetValue(update, updateEnabled ? "off" : "on");
-
-                profileSettingsContactSupportProp.SetValue(settings, contact_support);
-                profileSettingsReportProblemProp.SetValue(settings, report_problem);
-                profileSettingsUpdateProp.SetValue(settings, update);
-                profileSettingsNCInProp.SetValue(settings, nc_in);
-                profileSettingsNCOutProp.SetValue(settings, nc_out);
-
-                teamNameProp.SetValue(team, "SilveIT");
-                teamIDProp.SetValue(team, 1337U);
-
-                modePropsProp.SetValue(mode, "");
-                modeNameProp.SetValue(mode, "unlimited");
-
-                infoEmailProp.SetValue(userProfileInfo, "ModdedBy@SilveIT");
-                infoIdProp.SetValue(userProfileInfo, 1337U);
-                infoModeProp.SetValue(userProfileInfo, mode);
-                infoTeamProp.SetValue(userProfileInfo, team);
-                infoSettingsProp.SetValue(userProfileInfo, settings);
-                infoRefStringProp.SetValue(userProfileInfo, "SilveIT");
-
-                return MakeKrispResponse(userProfileInfo, R.T[16]);
+                var userProfileInfo = JsonSerializer.Deserialize(Resources.UserProfileInfo, R.T[0]);
+                return MakeKrispResponse(userProfileInfo);
             }
 
             /// <summary>
